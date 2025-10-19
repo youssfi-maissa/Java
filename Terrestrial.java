321f1489677078f728869f21dac011db7025aaa8
youssfi-maissa <Maissa.Yousfi@esprit.tn>
2025-10-19 13:53:43 +0100

    Create Terrestrial.java

---
+++ Terrestrial.java
@@ -0,0 +1,17 @@
+package zoo;
+
+public class Terrestrial extends Animal {
+    protected int nbrLegs;
+
+    public Terrestrial() {}
+
+    public Terrestrial(String family, String name, int age, boolean isMammal, int nbrLegs) {
+        super(family, name, age, isMammal);
+        this.nbrLegs = nbrLegs;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Pattes=" + nbrLegs;
+    }
+}
