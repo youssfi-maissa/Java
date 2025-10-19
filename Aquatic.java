6373792fcd7dec5bf6994443e27e09ad838abe01
youssfi-maissa <Maissa.Yousfi@esprit.tn>
2025-10-19 13:56:01 +0100

    Create Aquatic.java

---
+++ Aquatic.java
@@ -0,0 +1,21 @@
+package zoo;
+
+public class Aquatic extends Animal {
+    protected String habitat;
+
+    public Aquatic() {}
+
+    public Aquatic(String family, String name, int age, boolean isMammal, String habitat) {
+        super(family, name, age, isMammal);
+        this.habitat = habitat;
+    }
+
+    public void swim() {
+        System.out.println("This aquatic animal is swimming.");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Habitat=" + habitat;
+    }
+}
